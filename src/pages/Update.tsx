93c5614d73ea5c593e3b4db5d7485f4ac1dd06ef
DasunNirmal <dasun200301@gmail.com>
2025-01-07 22:07:24 +0530

    Done

--- src/pages/Update.tsx
+++ src/pages/Update.tsx
@@ -1,6 +1,5 @@
 import {useContext, useState} from "react";
 import {Customer} from "../models/Customer.ts";
-import {ItemContext} from "../store/ItemProvider.tsx";
 import {Item} from "../models/Item.ts";
 import {CustomerModal} from "../component/CustomerModal.tsx";
 import {ItemModal} from "../component/ItemModal.tsx";
@@ -8,12 +7,16 @@ import {CustomerTable} from "../component/CustomerTable.tsx";
 import {ItemTable} from "../component/ItemTable.tsx";
 import {useDispatch, useSelector} from "react-redux";
 import {updateCustomers} from "../reducers/CustomerSlice.ts";
+import {updateItems} from "../reducers/ItemSlice.ts";
 
 export default function Update() {
 
     const customers = useSelector(state => state.customer.value)
     const dispatchCustomer = useDispatch();
-    const [items, dispatchItem] = useContext(ItemContext);
+
+    const items = useSelector(state => state.item.value);
+    const dispatchItem = useDispatch();
+
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
@@ -27,9 +30,9 @@ export default function Update() {
         dispatchCustomer(updateCustomers({ name: customer.name, email: customer.email, phone: customer.phone }));
     }
 
-    function updateItems() {
-        const newItem = new Item(code, itemName, Number(qty));
-        dispatchItem({type: 'UPDATE_ITEM', payload: newItem});
+    function updateItem() {
+        const item = new Item(code, itemName, Number(qty));
+        dispatchItem(updateItems({code: item.code, itemName: item.itemName, qty: item.qty}));
     }
 
     function getTableDataCustomers(cell) {
@@ -58,7 +61,7 @@ export default function Update() {
             <div className="right-card m-3">
                 <h2 className="mb-6 p-2 w-fit text-2xl">Update Items</h2>
 
-                <ItemModal handleSubmit={updateItems} setCode={setCode} setItemName={setItemName} setQty={setQty} code={code} itemName={itemName} qty={qty}>Add Item</ItemModal>
+                <ItemModal handleSubmit={updateItem} setCode={setCode} setItemName={setItemName} setQty={setQty} code={code} itemName={itemName} qty={qty}>Update Item</ItemModal>
                 <ItemTable items={items} getTableDataItems={getTableDataItems}></ItemTable>
             </div>
         </div>
