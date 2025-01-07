84c09d63b4a36bd5da051aa59a615d3e116ea1d0
DasunNirmal <dasun200301@gmail.com>
2025-01-07 21:58:14 +0530

    Updated

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import CustomerSlice from "../reducers/CustomerSlice.ts";
+import itemSlice from "../reducers/ItemSlice.ts";
 
 export const store = configureStore({
     reducer: {
-        customer: CustomerSlice
+        customer: CustomerSlice,
+        item: itemSlice
     }
 });
