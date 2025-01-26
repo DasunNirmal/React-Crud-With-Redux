29a9c99b299f24388b8a1b6cf1e62f95c52e215e
DasunNirmal <dasun200301@gmail.com>
2025-01-26 13:07:18 +0530

    customers and items done

--- src/pages/Save.tsx
+++ src/pages/Save.tsx
@@ -6,16 +6,17 @@ import {ItemModal} from "../component/ItemModal.tsx";
 import {CustomerTable} from "../component/CustomerTable.tsx";
 import {ItemTable} from "../component/ItemTable.tsx";
 import {useDispatch, useSelector} from "react-redux";
-import {saveCustomers} from "../reducers/CustomerSlice.ts";
-import {saveItems} from "../reducers/ItemSlice.ts";
+import {saveCustomer} from "../reducers/CustomerSlice.ts";
+import {saveItem} from "../reducers/ItemSlice.ts";
+import {AppDispatch} from "../store/Store.ts";
 
 export default function Save() {
 
-    const customers = useSelector(state => state.customer.value)
-    const dispatchCustomer = useDispatch();
+    const dispatchCustomer = useDispatch<AppDispatch>();
+    const customers = useSelector((state: { customer: Customer[] }) => state.customer);
 
-    const items = useSelector(state => state.item.value);
-    const dispatchItem = useDispatch();
+    const dispatchItem = useDispatch<AppDispatch>();
+    const items = useSelector((state: { item:  Item[] }) => state.item);
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -29,15 +30,16 @@ export default function Save() {
         const customer = new Customer(name, email, phone);
         // Dispatch a plain object because Redux Toolkit expects plain JavaScript objects for actions and state.
         // Other wise it will give a error "A non-serializable value was detected in an action"
-        dispatchCustomer(saveCustomers({ name: customer.name, email: customer.email, phone: customer.phone }));
+        // saveCustomers({ name: customer.name, email: customer.email, phone: customer.phone })
+        dispatchCustomer(saveCustomer(customer));
     }
 
-    function addItem() {
+    function addItems() {
         const item = new Item(code, itemName, Number(qty));
-        dispatchItem(saveItems({code: item.code, itemName: item.itemName, qty: item.qty}));
+        dispatchItem(saveItem(item));
     }
 
-    function getTableDataCustomers(cell) {
+    function getTableDataCustomers(cell: Customer) {
         setName(cell.name);
         setEmail(cell.email);
         setPhone(cell.phone);
@@ -63,7 +65,7 @@ export default function Save() {
             <div className="right-card m-3">
                 <h2 className="mb-6 p-2 w-fit text-2xl">Save Items</h2>
 
-                <ItemModal handleSubmit={addItem} setCode={setCode} setItemName={setItemName} setQty={setQty} code={code} itemName={itemName} qty={qty}>Add Item</ItemModal>
+                <ItemModal handleSubmit={addItems} setCode={setCode} setItemName={setItemName} setQty={setQty} code={code} itemName={itemName} qty={qty}>Add Item</ItemModal>
                 <ItemTable items={items} getTableDataItems={getTableDataItems}></ItemTable>
             </div>
         </div>
