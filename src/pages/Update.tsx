1bc78b59a25ab4cd1a4816bd28f09dc8e756ffec
DasunNirmal <dasun200301@gmail.com>
2025-01-26 13:07:30 +0530

    customers and items done

--- src/pages/Update.tsx
+++ src/pages/Update.tsx
@@ -1,4 +1,4 @@
-import {useContext, useState} from "react";
+import {useState} from "react";
 import {Customer} from "../models/Customer.ts";
 import {Item} from "../models/Item.ts";
 import {CustomerModal} from "../component/CustomerModal.tsx";
@@ -6,16 +6,17 @@ import {ItemModal} from "../component/ItemModal.tsx";
 import {CustomerTable} from "../component/CustomerTable.tsx";
 import {ItemTable} from "../component/ItemTable.tsx";
 import {useDispatch, useSelector} from "react-redux";
-import {updateCustomers} from "../reducers/CustomerSlice.ts";
-import {updateItems} from "../reducers/ItemSlice.ts";
+import {updateCustomer} from "../reducers/CustomerSlice.ts";
+import {updateItem} from "../reducers/ItemSlice.ts";
+import {AppDispatch} from "../store/Store.ts";
 
 export default function Update() {
 
-    const customers = useSelector(state => state.customer.value)
-    const dispatchCustomer = useDispatch();
+    const dispatchCustomer = useDispatch<AppDispatch>();
+    const customers = useSelector((state: { customer: Customer[] }) => state.customer);
 
-    const items = useSelector(state => state.item.value);
-    const dispatchItem = useDispatch();
+    const dispatchItem = useDispatch<AppDispatch>();
+    const items = useSelector((state: { item:  Item[] }) => state.item);
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -25,14 +26,14 @@ export default function Update() {
     const [itemName, setItemName] = useState("");
     const [qty, setQty] = useState("");
 
-    function updateCustomer() {
+    function updateCustomers() {
         const customer = new Customer(name, email, phone);
-        dispatchCustomer(updateCustomers({ name: customer.name, email: customer.email, phone: customer.phone }));
+        dispatchCustomer(updateCustomer(customer));
     }
 
-    function updateItem() {
+    function updateItems() {
         const item = new Item(code, itemName, Number(qty));
-        dispatchItem(updateItems({code: item.code, itemName: item.itemName, qty: item.qty}));
+        dispatchItem(updateItem(item));
     }
 
     function getTableDataCustomers(cell) {
@@ -53,7 +54,7 @@ export default function Update() {
             <div className="left-card m-3">
                 <h2 className="mb-6 p-2 w-fit text-2xl">Update Customer</h2>
 
-                <CustomerModal handleSubmit={updateCustomer} setName={setName} setEmail={setEmail} setPhone={setPhone} name={name} email={email} phone={phone}>Update Customer</CustomerModal>
+                <CustomerModal handleSubmit={updateCustomers} setName={setName} setEmail={setEmail} setPhone={setPhone} name={name} email={email} phone={phone}>Update Customer</CustomerModal>
                 <CustomerTable customers={customers} getTableData={getTableDataCustomers}></CustomerTable>
             </div>
 
@@ -61,7 +62,7 @@ export default function Update() {
             <div className="right-card m-3">
                 <h2 className="mb-6 p-2 w-fit text-2xl">Update Items</h2>
 
-                <ItemModal handleSubmit={updateItem} setCode={setCode} setItemName={setItemName} setQty={setQty} code={code} itemName={itemName} qty={qty}>Update Item</ItemModal>
+                <ItemModal handleSubmit={updateItems} setCode={setCode} setItemName={setItemName} setQty={setQty} code={code} itemName={itemName} qty={qty}>Update Item</ItemModal>
                 <ItemTable items={items} getTableDataItems={getTableDataItems}></ItemTable>
             </div>
         </div>
