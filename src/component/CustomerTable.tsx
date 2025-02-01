887a4d9c04fa1f7c14a332cf503b8c63b5ae7cd3
DasunNirmal <dasun200301@gmail.com>
2025-02-01 21:52:15 +0530

    finished

--- src/component/CustomerTable.tsx
+++ src/component/CustomerTable.tsx
@@ -1,6 +1,6 @@
 import {Customer} from "../models/Customer.ts";
 
-export function CustomerTable(props,children) {
+export function CustomerTable(props) {
     return (
         <>
             <table className="table-auto border border-gray-300 w-full mt-6">
@@ -8,12 +8,12 @@ export function CustomerTable(props,children) {
                 <tr>
                     <td>Name</td>
                     <td>Email</td>
-                    <td>phone</td>
+                    <td>Phone</td>
                 </tr>
                 </thead>
                 <tbody>
-                {props.customers.map((customer: Customer) => (
-                    <tr key={customer.email} onClick={() => {
+                {props.customers.map((customer: Customer,index: number) => (
+                    <tr key={index} onClick={() => {
                         props.getTableData(customer)
                     }}>
                         <td>{customer.name}</td>
